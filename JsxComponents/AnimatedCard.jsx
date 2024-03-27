0c01f984b024ab0fb607679eb583787b19089b32
Srinath10X <srinathcreates@gmail.com>
2024-03-27 23:46:14 +0530

    Added Animated card component

---
+++ JsxComponents/AnimatedCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AnimatedCard.css";
+
+const AnimatedCard = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="card">
+          <h1>Hello</h1>
+          <p>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AnimatedCard;
