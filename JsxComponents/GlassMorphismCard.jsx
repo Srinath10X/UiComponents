450e8b5cefad440d42cec87937b3853325d4ecdd
Srinath10X <srinathcreates@gmail.com>
2024-03-27 23:49:20 +0530

    Added glassmorphism card component

---
+++ JsxComponents/GlassMorphismCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./GlassMorphismCard.css";
+
+const GlassMorphismCard = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="card">
+          <h1>Ui Glassmorphism card</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GlassMorphismCard;
