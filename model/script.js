6c682d8338a590ad0823be8334a2cb88b8166001
Srinath10X <srinathcreates@gmail.com>
2024-04-01 22:18:01 +0530

    added new dialog component

---
+++ model/script.js
@@ -0,0 +1,11 @@
+const model = document.querySelector("[dialog-model]");
+
+// openButton
+const openButton = () => {
+  model.showModal();
+};
+
+// closeButton
+const closeButton = () => {
+  model.close();
+};
